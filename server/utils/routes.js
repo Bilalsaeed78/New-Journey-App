4f6016bf61a6744d5f24252461c297435c6e7e63
Ali Asar Khowaja <khowajaaliasar@gmail.com>
2024-06-30 21:20:46 +0500

    Removed extra controllers.

--- server/utils/routes.js
+++ server/utils/routes.js
@@ -5,7 +5,6 @@ const propertyRoutes = require('../routes/propertyRoutes');
 const officeRoutes = require('../routes/officeRoutes');
 const apartmentRoutes = require('../routes/apartmentRoutes');
 const roomRoutes = require('../routes/roomRoutes');
-const bookingRoutes = require('../routes/bookingRoutes');
 const reviewRoutes = require('../routes/reviewRoutes');
 const locationFilterRoutes = require('../routes/locationFilterRoutes');
 const requestRoutes = require('../routes/requestRoutes');
@@ -18,7 +17,6 @@ module.exports = function (app){
     app.use("/api/office", officeRoutes);
     app.use("/api/apartment", apartmentRoutes);
     app.use("/api/room", roomRoutes);
-    app.use("/api/booking", bookingRoutes);
     app.use("/api/review", reviewRoutes);
     app.use("/api/filter", locationFilterRoutes);
     app.use("/api/request", requestRoutes);
